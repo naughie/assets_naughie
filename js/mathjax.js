ca0791cd658b93be2527899dd549f813f8efaae4
Naughie <zomacchi@gmail.com>
2019-05-21 16:34:43 +0900

    Add mathjax config

---
+++ js/mathjax.js
@@ -0,0 +1,20 @@
+MathJax.Hub.Config({
+  tex2jax: {
+    inlineMath: [['$','$'], ['\\(','\\)']],
+  },
+  displayAlign: "left",
+  displayIndent: "2em",
+  TeX: {
+    equationNumbers: {
+      autoNumber: "AMS",
+    },
+    TagSide: "left",
+    extensions: [
+      "AMScd.js",
+    ],
+  },
+  CommonHTML: {
+    matchFontHeight: false,
+    mtextFontInherit: true,
+  },
+});
